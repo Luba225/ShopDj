93f82bd998c37a0e070bd527aa942927cf9f4b0f
Luba225 <145285107+Luba225@users.noreply.github.com>
2025-10-25 21:58:12 +0300

    shop

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
-from . import views
-
-app_name = 'main'
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.product_list, name='product-list'),
-    
-    path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-
-    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product-detail'),  
+    path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace='main')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
