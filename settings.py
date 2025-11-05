a26a55d4cd14c48d3112079f2c9646feb348446b
Luba225 <145285107+Luba225@users.noreply.github.com>
2025-11-05 16:48:29 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 BASE_DIR = Path(__file__).resolve().parent
 
 SECRET_KEY = 'django-insecure-your-secret-key'
@@ -14,6 +16,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'accounts.apps.AccountsConfig',
+    'ckeditor',
+    'ckeditor_uploader',
+    'reviews',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -40,6 +49,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -63,6 +73,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.parent / 'media'
@@ -74,3 +85,29 @@ LOGIN_URL = 'accounts:login'
 # LOGOUT_REDIRECT_URL = 'main:product_list'
 LOGIN_REDIRECT_URL = '/' 
 LOGOUT_REDIRECT_URL = '/'
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+
+# # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# # EMAIL_HOST = 'smtp.gmail.com'
+# # EMAIL_PORT = 587
+# # EMAIL_USE_TLS = True
+# # EMAIL_HOST_USER = 'your_email@gmail.com' 
+# # EMAIL_HOST_PASSWORD = 'your_app_password' 
+
+DEFAULT_FROM_EMAIL = 'support@myshop.com'
+
+# Замінити ці значення на реальні, коли будете використовувати LiqPay
+# Для симуляції:
+
+LIQPAY_PUBLIC_KEY = 'sandbox_i0000000000' 
+LIQPAY_PRIVATE_KEY = 'sandbox_p0000000000' 
+LIQPAY_API_URL = 'https://www.liqpay.ua/api/'
+# URL для відправки форми (для sandbox)
+LIQPAY_SEND_URL = 'https://sandbox.liqpay.ua/api/3/checkout'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
